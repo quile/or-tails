5eb13b9e83459cb6af6eb1568e13d3a6f0812288
Kyle Dawkins <kyle.dawkins@weareinstrument.com>
2013-03-29 16:53:10 -0700

    Added method to find dogs in a park

--- app/www/js/map.js
+++ app/www/js/map.js
@@ -6,15 +6,29 @@ var view_map = function() {
 	function createMap(){
 
 		console.log("Creating map?");
-		
+		        
 		var mapOptions = {
-	      center: new google.maps.LatLng(45.52594, -122.65595),
+	      center: new google.maps.LatLng(),
 	          zoom: 14,
 	          disableDefaultUI: true,
 	          mapTypeId: google.maps.MapTypeId.ROADMAP
 	    };
-	        
+	       
 	    map = new google.maps.Map( $("#map-container").get(0), mapOptions );
+        
+        // locate user and move map
+        var me = Parse.User.current();
+        var currentPosition = navigator.geolocation.getCurrentPosition(
+            function(point) {
+                map.setCenter( new google.maps.LatLng( point.coords.latitude, point.coords.longitude ) );
+                me.setLocation( point.coords.latitude, point.coords.longitude, function(error, result) {
+                    console.log("Changed user's location to " + point.coords.latitude + " / " + points.coords.longitude );
+                });
+            },
+            function(error) {
+                console.log("Couldn't get GPS position");
+            }
+        );
 	}
 
 	return {
@@ -22,6 +36,16 @@ var view_map = function() {
 
 		initialize : function() {
 			createMap();
+            
+            if (!me) {
+                app.showView("login");
+            }
+            
+            $("check-in").click(function(el) {
+                var self = this;
+                e.preventDefault();
+                self.checkIn();
+            });
 		},
 
 		show : function(params) {
@@ -32,6 +56,15 @@ var view_map = function() {
 
 		getState : function() {
 			return {};
-		}
+		},
+        
+        checkIn: function() {
+            var me = Parse.User.current();
+            if (!me) {
+                app.showView("login");
+            }
+            var location = me.location();
+            
+        }
 	} 
 };
